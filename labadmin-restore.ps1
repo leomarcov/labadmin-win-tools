4df91c0e64be9247d1e319c5ca67f18a12496a3a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-29 23:28:36 +0100

    Update labadmin-restore.ps1

--- labadmin-restore.ps1
+++ labadmin-restore.ps1
@@ -1,4 +1,4 @@
 #Requires -RunAsAdministrator
 
 $conf = Get-Content ${PSScriptRoot}/conf | ConvertFrom-StringData
-Restore-Computer-RestorePoint $conf.main_rp
+Restore-Computer -RestorePoint $conf.main_rp
