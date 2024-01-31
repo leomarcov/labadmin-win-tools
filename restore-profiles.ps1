558558410e785b5c4785200fa21ac3ddf90a1f8b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-31 16:34:40 +0100

    Update restore-profiles.ps1

--- restore-profiles.ps1
+++ restore-profiles.ps1
@@ -1,15 +1,25 @@
 # CONFIG VARIABLES
 $restore_users="alumno","pepe"
-$backup_folder="C:\Users\profiles-backup\"
-$restore_days_file="restore-days.conf"
+$backups_path="C:\Users\restore-profile\"
+$restore_cache_filename="restore-profile.conf"
 $default_restore_days="1"
 
 # CREATE BACKUP
 foreach(user in $resore_users) {
-  robocopy "C:\Users\${u}" "${backup_folder}\${u}" /MIR /XJ /COPYALL
+  robocopy "C:\Users\${u}" "${backups_path}\${u}" /MIR /XJ /COPYALL
 }
 
 # RESTORE
-foreach(user in $resore_users) {
-  echo d | robocopy "${backup_folder}\${u}" "C:\Users\${u}" /MIR /XJ /COPYALL /XD "${backup_folder}\${u}\Desktop" /XF "${backup_folder}\${u}\${restore_days_file}" /XD "${backup_folder}\${u}\Downloads" /XD "${backup_folder}\${u}\Documents"
+foreach(user in $restore_users) {
+  $user_profile="C:\Users\${u}"
+  $user_backup="${backups_path}\${u}"
+  
+  # Each call restore
+  echo d | robocopy "${user_backup}\Appdata\Local\Google\Chrome" "${user_profile}\AppData\Local\Google\Chrome" /MIR /XJ /COPYALL 
+  echo d | robocopy "${user_backup}\Appdata\Local\Mozilla\Firefox" "${user_profile}\AppData\Local\Mozilla\Firefox" /MIR /XJ /COPYALL 
+
+  $user_conf = Get-Content $b| ConvertFrom-StringData
+  $user_restore_conf
+  # Scheduled restore
+
 }
