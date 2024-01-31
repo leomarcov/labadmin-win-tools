f6a9c8c522246ab2c4fc5afe4b4afe4625f51444
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-31 13:12:19 +0100

    Update restore-profiles.ps1

--- restore-profiles.ps1
+++ restore-profiles.ps1
@@ -1,9 +1,13 @@
 # CONFIG VARIABLES
-$restore_users="pepe"
+$restore_users="alumno","pepe"
 $backup_folder="C:\Users\profiles-backup\"
-
+$restore_days_file="restore-days.conf"
 
 
 robocopy c:\users\pepe c:\users\pepe_bak /mir /xj /copyall
 
-echo d | robocopy c:\users\pepe_bak2 c:\users\pepe /mir /xj /copyall /xd c:\users\pepe_bak2\desktop /xd c:\users\pepe_bak2\downloads
+
+# RESTORE
+foreach(user in $resore_users) {
+  echo d | robocopy "${backup_folder}\${u}" "C:\Users\${u}" /MIR /XJ /COPYALL /XD "${backup_folder}\${u}\Desktop" /XD "${backup_folder}\${u}\Downloads" /XD "${backup_folder}\${u}\Documents"
+}
