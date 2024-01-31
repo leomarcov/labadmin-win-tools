abecf86b0ca93c999835aac500259e2ae263adbd
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-31 13:03:50 +0100

    Update and rename restore-profile.ps1 to restore-profiles.ps1

--- restore-profiles.ps1
+++ restore-profiles.ps1
@@ -1,3 +1,9 @@
+# CONFIG VARIABLES
+$restore_users="pepe"
+$backup_folder="C:\Users\profiles-backup\"
+
+
+
 robocopy c:\users\pepe c:\users\pepe_bak /mir /xj /copyall
 
 echo d | robocopy c:\users\pepe_bak2 c:\users\pepe /mir /xj /copyall /xd c:\users\pepe_bak2\desktop /xd c:\users\pepe_bak2\downloads
