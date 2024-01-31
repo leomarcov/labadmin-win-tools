b2bc564a32ebd3326f230a7c77a0ee08b9c71795
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-31 18:07:29 +0100

    Update restore-profiles.ps1

--- restore-profiles.ps1
+++ restore-profiles.ps1
@@ -1,12 +1,28 @@
-# CONFIG VARIABLES
-$users="alumno","pepe"
+###################################################################
+#### CONFIG VARIABLES
+###################################################################
+$fixed_users="alumno","pepe"
 $backups_path="C:\Users\restore-profile\"
 
-# CREATE BACKUP
+###################################################################
+#### PARAMETERS
+###################################################################
+Param(
+  [switch]$backup,        # Backup profiles
+  [switch]$restore        # Restore profiles
+  [String]$users          # List of users to backup/restore instead of $fixed_users
+)
+
+if(!$users) { $users=$fixed_users }
+
+###################################################################
+#### CREATE BACKUP
+###################################################################
 foreach(user in $users) {
   $user_profile="C:\Users\${u}"
   $user_backup="${backups_path}\${u}"
-
+  
+  New-Item -ItemType Directory -Force -Path $bachups_path
   robocopy $user_profile $user_backup /MIR /XJ /COPYALL
 }
 
