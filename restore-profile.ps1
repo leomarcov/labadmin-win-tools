bf32c7d9741d077cf90cd98dfbdef308be1a106b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-01-31 12:08:50 +0100

    Create restore-profile.ps1

---
+++ restore-profile.ps1
@@ -0,0 +1,3 @@
+robocopy c:\users\pepe c:\users\pepe_bak /mir /xj /copyall
+
+echo d | robocopy c:\users\pepe_bak2 c:\users\pepe /mir /xj /copyall /xd c:\users\pepe_bak2\desktop /xd c:\users\pepe_bak2\downloads
