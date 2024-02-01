9eacb0bc28667d014b96763a335237154868a20d
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-01 07:26:14 +0100

    Update config-usbstorage.ps1

--- config-usbstorage.ps1
+++ config-usbstorage.ps1
@@ -6,9 +6,11 @@ Param(
 # DISABLE
 if($disable) {
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "start" -Value 4
+
 # ENABLE
 } elseif($enable) 
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "start" -Value 3
+
 # SHOW CURRENT STATE
 } else {
   if ((Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\' -Name "start") -eq 4) { Write-Output "Disabled" } 
