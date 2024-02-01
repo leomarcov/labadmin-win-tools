b8e978714269f3f18c3d5865725a22d48f43fa29
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-01 08:15:47 +0100

    Update clean-profiles.ps1

--- clean-profiles.ps1
+++ clean-profiles.ps1
@@ -30,12 +30,17 @@ $backups_path="C:\Users\restore-profile"
 if(!$users) { $users=$fixed_users }
 
 
+
 #### CREATE PROFILE BACKUP #######################################
 if($CreateBackup) {
   New-Item -ItemType Directory -Force -Path $backups_path | Out-Null   # Create backups path if no exists
   foreach($u in $users) {
     $user_profile="C:\Users\${u}"
     $user_backup="${backups_path}\${u}"
+
+    if(!Test-Path $user_profile) { Write-Output "Folder $user_profile not exists. Skipping user $u"; continue }
+    if(Test-Path $user_backup) { Remove-Item -Recurse -Force $user_backup }
+    
     robocopy $user_profile $user_backup /MIR /XJ /COPYALL
   }
   exit
@@ -47,25 +52,25 @@ foreach($u in $users) {
   $user_profile="C:\Users\${u}"
   $user_backup="${backups_path}\${u}"
 
-  if(!Test-Path $user_profile) {
-    Write-Output "Folder $user_profile not exists. Skipping user $u"
-    continue
-  }
+  if(!Test-Path $user_profile) { Write-Output "Folder $user_profile not exists. Skipping user $u"; continue }
+  if(!Test-Path $user_backup)  { Write-Output "Folder $user_profile not exists. Skipping user $u"; continue }
   
-  # Every call restore:
+  # RESTORE EVERY CALL
   echo d | robocopy "${user_backup}\Appdata\Local\Google\Chrome" "${user_profile}\AppData\Local\Google\Chrome" /MIR /XJ /COPYALL 
   echo d | robocopy "${user_backup}\Appdata\Local\Mozilla\Firefox" "${user_profile}\AppData\Local\Mozilla\Firefox" /MIR /XJ /COPYALL 
 
-  # Scheduled restore:
-  $user_conf = Get-Content "${user_backup}\restore-profile.conf"| ConvertFrom-StringData
+  # SCHEDULED RESTORE
+  $user_conf = Get-Content "${user_backup}\clean-profile.conf"| ConvertFrom-StringData
+  $hashtable.GetEnumerator()|select name,value|convertto-csv | out-file file.csv
 
-  
-  $user_conf = Get-Content "${user_backup}\restore-profile.conf"| ConvertFrom-StringData
+  $user_conf = Get-Content "${user_backup}\clean-profile.conf"| ConvertFrom-StringData
   $user_restore_conf
 
   if((New-TimeSpan -Start ([DateTime]$user_conf.lastRestore) -End (Get-Date)).Days -ge 1) {
-    echo d | robocopy "${user_backup}\" "${user_profile}" /MIR /XJ /COPYALL 
+    echo d | robocopy ${user_backup} ${user_profile} /MIR /XJ /COPYALL /XF "${user_backup}\clean-profile.conf"
+    $user_conf.last=Get-Date -Format "yyyy-MM-dd"
   }
 }
+
 
 
