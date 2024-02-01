2c737f681446bbd417560e3939e710162fb130ec
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-01 07:16:01 +0100

    Update manage-restorepoints.ps1

--- manage-restorepoints.ps1
+++ manage-restorepoints.ps1
@@ -2,21 +2,30 @@
 
 #### PARAMETERS ##################################################
 Param(
+  [Switch]$Enable,           # Enable RPs
   [Switch]$Create,           # Create RP
   [Switch]$DeleteAll,        # Delete ALL restore points
   [Switch]$Restore           # Restore RP
 )
 
 
+#### ACTION ENABLE ###############################################
+} if($enable) {
+    Enable-ComputerRestore -Drive c:
+    vssadmin resize shadowstorage /for=C: /on=C: /maxsize=5GB
 
 #### ACTION CREATE ###############################################
-if$($create) {
+elseif$($create) {
     Checkpoint-Computer -Description "labadmin-freezer-main"
+
+#### ACTION DELETE ###############################################
 } elseif($deleteall) {
-  
+  vssadmin delete shadows /all
+
 #### ACTION RESTORE ###############################################
 } elseif($restore) {
     Restore-Computer -RestorePoint 1
+
 #### ACTION LIST ##################################################
 } else {
    Get-ComputerRestorePoint
