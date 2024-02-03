632445cf1d6e4f8508e0608a76b359c2cb783c04
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-03 08:57:45 +0100

    Update profiles-cleaner.ps1

--- profiles-cleaner.ps1
+++ profiles-cleaner.ps1
@@ -53,13 +53,11 @@ if($CreateBackup) {
     New-Item -ItemType Directory -Force -Path $backups_path | Out-Null   
     attrib +h $backups_path
     $acl = Get-Acl $backups_path
-    $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($acl.owner,"FullControl","Allow")
-    $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($acl.owner)))
-    $acl.SetAccessRuleProtection($true,$false)
-    $acl.SetAccessRule($accessRule)
-    $acl | Set-Acl $backups_path
-
-    (New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
+    $acl.SetAccessRuleProtection($true, $false)
+    $adminsgrp_name=(New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
+    $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($adminsgrp_name)))
+    $acl.SetAccessRule(New-Object System.Security.AccessControl.FileSystemAccessRule($adminsgrp_name,"FullControl","Allow"))
+    Set-Acl -Path $backups_path -AclObject $acl
   }
 
   foreach($u in $users) {
