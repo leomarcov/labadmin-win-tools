f34155ed21370cb7375fedd3e44e8eb8bb7c2744
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-03 10:18:51 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,12 +5,7 @@ $install_path=$ENV:ProgramFiles+"\labadmin\labadmin-freezer\"
 
 
 # Create folder and close permissions to admin
-if(!(Test-Path $install_path)) {
-  New-Item -ItemType Directory -Force -Path $install_path | Out-Null   
-
-}
-
-
+if(!(Test-Path $install_path)) { New-Item -ItemType Directory -Force -Path $install_path | Out-Null }
 
 # Download files
 $url="https://raw.githubusercontent.com/leomarcov/labadmin-freezer/main/"
@@ -18,9 +13,3 @@ $url="https://raw.githubusercontent.com/leomarcov/labadmin-freezer/main/"
 $url_files="config-usbstorage.ps1 manage-restorepoints.ps1 profiles-cleaner.ps1"
 foreach($f in $url_files.split(" ")) { Invoke-WebRequest -Uri ("${url}/${f}") -OutFile ($install_path+"\$f") }
 
-  $acl = Get-Acl $install_path
-  $acl.SetAccessRuleProtection($true,$false)
-  $adminsgrp_name=(New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
-  $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($adminsgrp_name)))
-  $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($adminsgrp_name,"FullControl","Allow")))
-  Set-Acl -Path $install_path -AclObject $acl
