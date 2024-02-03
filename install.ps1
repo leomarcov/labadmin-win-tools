aeb007704a345a2e3da2b48f8553d86554a9b009
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-03 09:06:48 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,7 +10,7 @@ if(!(Test-Path $install_path)) {
   $adminsgrp_name=(New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
   $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($adminsgrp_name)))
   $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($adminsgrp_name,"FullControl","Allow")))
-  Set-Acl -Path $backups_path -AclObject $acl
+  Set-Acl -Path $install_path -AclObject $acl
 }
 
 
