54b28e9e50cef294f5b03c4022bb9b7604277ab5
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-03 17:06:01 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,4 +10,5 @@ $url="https://raw.githubusercontent.com/leomarcov/labadmin-freezer/main/"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url_files="config-usbstorage.ps1 manage-restorepoints.ps1 profiles-cleaner.ps1 install.ps1"
 foreach($f in $url_files.split(" ")) { Invoke-WebRequest -Uri ("${url}/${f}") -OutFile ($install_path+"\$f") }
+Rename-Item -Path $install_path -NewName update.ps1
 
