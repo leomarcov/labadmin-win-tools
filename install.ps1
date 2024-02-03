448b78170333c9eea54253460ca5271ff7a289b4
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-03 09:30:25 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,19 +2,12 @@
 
 $install_path=$ENV:ProgramFiles+"\labadmin\labadmin-freezer\"
 
-if ((New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $false) {
-    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
+
 
 # Create folder and close permissions to admin
 if(!(Test-Path $install_path)) {
   New-Item -ItemType Directory -Force -Path $install_path | Out-Null   
-  $acl = Get-Acl $install_path
-  $acl.SetAccessRuleProtection($true,$false)
-  $adminsgrp_name=(New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
-  $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($adminsgrp_name)))
-  $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($adminsgrp_name,"FullControl","Allow")))
-  Set-Acl -Path $install_path -AclObject $acl
+
 }
 
 
@@ -25,3 +18,9 @@ $url="https://raw.githubusercontent.com/leomarcov/labadmin-freezer/main/"
 $url_files="config-usbstorage.ps1 manage-restorepoints.ps1 profiles-cleaner.ps1"
 foreach($f in $url_files.split(" ")) { Invoke-WebRequest -Uri ("${url}/${f}") -OutFile ($install_path+"\$f") }
 
+  $acl = Get-Acl $install_path
+  $acl.SetAccessRuleProtection($true,$false)
+  $adminsgrp_name=(New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-544').Translate([type]'System.Security.Principal.NTAccount').value
+  $acl.SetOwner((New-Object System.Security.Principal.Ntaccount($adminsgrp_name)))
+  $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($adminsgrp_name,"FullControl","Allow")))
+  Set-Acl -Path $install_path -AclObject $acl
