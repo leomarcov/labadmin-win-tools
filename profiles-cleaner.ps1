150c1f413cca4c618207401eb866bcbe0e96f180
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-04 12:04:07 +0100

    Update profiles-cleaner.ps1

--- profiles-cleaner.ps1
+++ profiles-cleaner.ps1
@@ -3,46 +3,59 @@
 <#
 .SYNOPSIS
     Automated user profiles cleaner
+
 .DESCRIPTION
-    Automated user profiles cleaner for backup and restore at startup 
+    Automated user profiles cleaner for backup and autorestore at startup 
     Each profile is backup in c:\users\profiles-cleaner\ and a username.cfg file is generated
     Profile config file syntax is:
         cleanAfterDays                      # Number of days until autoclean
-        skip                                # Skip autoclean for this user
+        skipUser                            # Skip autoclean for this user
         cleanAlways                         # Array of relative paths to clean on every call
         lastClean                           # Date when last clean was performed
+
 .PARAMETER CreateBackup
     Backup (or update backup if previos backup exists) current users profiles to c:\users\profiles-cleaner\
-    If no use this parameter, instead of backup, profile restore (clean) is done
     Parameter -users must be given with list of users to backup
     For new backups default username.cfg file is generated
+
+.PARAMETER RestoreProfiles
+    Try to restore profiles according user backup profile config
+
 .PARAMETER Users
     List of users to backup/restore
     With -CreateBackup this parameter is mandatory
     When restore is optional. If no given all backup profiles stored are used
+
 .PARAMETER Force
-    Force profile clean and ommits skip user config
+    Force profile clean and ommits skipUser and cleanAfterDays config
+
 .PARAMETER Log
     Save output to log file in c:\users\profiels-cleaner\log.txt
+
 .NOTES
     File Name      : profiles-cleaner.ps1
     Author         : Leonardo Marco
-.EXAMPLE
-    profiles-cleaner.ps1 -CreateBackup -Users user1,user2,user3
-.EXAMPLE
-    profiles-cleaner.ps1
-    profiles-cleaner.ps1 -Users user1
-    profiles-cleaner.ps1 -Force
 #>
 
 
 #### PARAMETERS ##################################################
 Param(
-  [Switch]$CreateBackups,            # Backup profiles instead of restore
-  [String[]]$Users,                  # Optional list of users to backup or restore instead of .cfg files
-  [Switch]$Force,                    # Force restore instead of config file dates
-  [Switch]$Log                       # Save to log file
+  [parameter(Mandatory=$true, ParameterSetName="create")]
+  [Switch]$CreateBackups,
+  
+  [parameter(Mandatory=$true, ParameterSetName="restore")]
+  [Switch]$RestoreProfiles,
+
+  [parameter(Mandatory=$true, ParameterSetName="create")]
+  [parameter(Mandatory=$false, ParameterSetName="restore")]
+  [String[]]$Users,
+
+  [parameter(ParameterSetName="restore")]
+  [Switch]$Force,
+
+  [Switch]$Log
 )
+
 
 #### CONFIG VARIABLES ############################################
 $backups_path="C:\Users\profiles-cleaner"                                                # Path to save backups and configs
@@ -51,16 +64,14 @@ $default_config=@{
     cleanAfterDays=1                                                                     # Days after spend to exec a new profile autoclean
     lastClean=(Get-Date -Format "yyy-MM-dd")                                             # Date of last autoclean executed
     skipUser=$false                                                                      # Skip this user of autoclean
-    cleanAllways=@("\Appdata\Local\Google\Chrome","\Appdata\Local\Mozilla\Firefox")     # Items inside profile user to clean on every call
+    cleanAllways=@("\Appdata\Local\Google\Chrome","\Appdata\Local\Mozilla\Firefox")      # Items inside profile user to clean on every call
 }
 
 
 
 #### FUNCTION CreateBackup #######################################
 function CreateBackups {
-  if(!$users) { Write-Output "Please, specific users parameter"; exit 1 }
-
-  # Create backups folder and set permissions
+  # Create backups folder and set Administrator permissions
   if(!(Test-Path $backups_path)) {
     New-Item -ItemType Directory -Force -Path $backups_path | Out-Null   
     attrib +h $backups_path
@@ -104,16 +115,16 @@ function RestoreProfiles {
 
       # CHECK FOLDER
       if(!(Test-Path $user_backup))  { Write-Output "WARNING! Folder $user_backup not exists. Skipping user $u"; continue }
-
+      
+      # SKIP USER (only if no force)
+      if(!$Force -AND $user_conf.skipUser -eq "true") { Write-Output "Skipping user $u for config file"; continue }
+      
       # GET USER CONFIG
       $user_conf=@{}; (Get-Content $user_conf_file | ConvertFrom-Json).psobject.properties | Foreach { $user_conf[$_.Name] = $_.Value }
       if($user_conf.cleanAfterDays -isnot [int] -OR !$user_conf.lastClean) {
         Write-Output "WARNING! Invalid config file ${user_conf_file}. Skipping user ${u}"
         continue
       }
-
-      # SKIP USER (only if no force)
-      if(!$Force -AND $user_conf.skipUser -eq "true") { Write-Output "Skipping user $u for config file"; continue }
 
       # SCHEDULED RESTORE
       if($Force -OR (New-TimeSpan -Start ([DateTime]$user_conf.lastClean) -End (Get-Date)).Days -ge $user_conf.cleanAfterDays) {
@@ -137,18 +148,21 @@ function RestoreProfiles {
 }
 
 
-#### FUNCTION main
+
+#### FUNCTION MAIN
 function main {
-    if($CreateBackups) {  CreateBackups  }
-    else               { RestoreProfiles }
+    if($CreateBackups)       { CreateBackups  }
+    elseif($RestoreProfiles) { RestoreProfiles }
 }
+
 
 # Exec 
 if(!$Log) { main }
+
 # Exec > log.txt
 else {
-    if((Get-ChildItem $logs_path | % {[int]($_.length / 1kb)}) -gt 4) { Remove-Item -Path $log_path }  # Delete log if size > 4kb
+    if((Get-ChildItem $logs_path | % {[int]($_.length / 1kb)}) -gt 8) { Remove-Item -Path $log_path }  # Delete log if size > 4kb
     &{ Write-Output "`n`n#########################################################################################################"(Get-Date).toString()"#########################################################################################################"; main } 2>&1 | Out-File -FilePath $log_path -Append 
 }
-      
+
 
