874fac7f538c5de57e05abf8b47b066c103c637b
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-05 15:07:53 +0100

    Update labadmin-utils.ps1

--- labadmin-utils.ps1
+++ labadmin-utils.ps1
@@ -1,1 +1,20 @@
 #Requires -RunAsAdministrator
+Param(
+  [Switch]$EnableUSBStorages,
+  [Switch]$DisableUSBStorages,
+  [Switch]$StatusUSBStorages,
+)
+
+# DISABLE
+if($EnableUSBStorages) {
+  Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 4
+
+# ENABLE 
+} elseif($DisableUSBSTorages) {
+  Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 3
+
+# SHOW CURRENT STATUS
+} elseif($StatusUSBStorages) {
+  if ((Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\' -Name "Start") -eq 4) { Write-Output "Disabled" } 
+  else { Write-Output "Enabled" }
+}
