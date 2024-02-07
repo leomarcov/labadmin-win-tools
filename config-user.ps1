3b6296a1e6eb54b45bb8220ebd03775f9370e729
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 11:27:10 +0100

    Update config-user.ps1

--- config-user.ps1
+++ config-user.ps1
@@ -1,11 +1,27 @@
-#Requires -RunAsAdministrator
+Requires -RunAsAdministrator
 
 Param(
-  [Parameter(Mandatory)]
+  [Parameter(Mandatory=$true)]
   [String]$UserName,
+
+  [parameter(Mandatory=$true, ParameterSetName='password')]
+  [Switch]$SetPassword,
+  [parameter(Mandatory=$true, ParameterSetName='password')]
+  [String]$Password,
+
+  [Parameter(ParameterSetName='nopass')]
+  [Switch]$NoPassword,
+
+  [Parameter(ParameterSetName='hide')]
   [Switch]$Hide,
+  
+  [Parameter(ParameterSetName='hide')]
   [Switch]$Unhide,
+  
+  [Parameter(ParameterSetName='disable')]
   [Switch]$Disable,
+  
+  [Parameter(ParameterSetName='disable')]
   [Switch]$Enable
 )
 
@@ -14,14 +30,23 @@ Get-LocalUser -Name $UserName -ErrorAction Stop | Out-Null
 if($Hide) {
   New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Force | New-ItemProperty -Name $UserName -Value 0 -PropertyType DWord -Force
 }
-if($Unhide) {
+elseif($Unhide) {
 	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $UserName -Force
 }
+
 if($Disable) {
 	Disable-LocalUser -Name $UserName
     Get-LocalUser -Name $UserName
 }
-if($Enable) {
+elseif($Enable) {
 	Enable-LocalUser -Name $UserName
     Get-LocalUser -Name $UserName
+}
+
+if($NoPassword) {
+	Set-LocalUser -Name $UserName -Password ([securestring]::new())
+}
+if($SetPassword){
+ 	$ss=$Password|ConvertTo-SecureString -AsPlainText -Force
+ 	Set-LocalUser -Name $UserName -Password $ss
 }
