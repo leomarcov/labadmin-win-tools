52dd746c6dc905d9f3db3103d882ab5417617e27
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 09:55:23 +0100

    Update config-usbstorage.ps1

--- config-usbstorage.ps1
+++ config-usbstorage.ps1
@@ -1,34 +1,17 @@
 #Requires -RunAsAdministrator
 
-<#
-.SYNOPSIS
-    Enables/disables USB Storages 
-.NOTES
-    File Name      : config-usbstorage.ps1
-    Author         : Leonardo Marco
-.EXAMPLE
-    config-usbstorage.ps1 -Enable
-.EXAMPLE
-    config-usbstorage.ps1 -Disable
-.EXAMPLE
-    config-usbstorage.ps1      # Show current status
-#>
-
 Param(
   [Switch]$Enable,
   [Switch]$Disable
 )
 
-# DISABLE
 if($disable) {
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 4
 
-# ENABLE 
 } elseif($enable) {
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 3
 
-# SHOW CURRENT STATUS
 } else {
-  if ((Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\' -Name "Start") -eq 4) { Write-Output "Current status: Disabled" } 
-  else { Write-Output "Current status: Enabled" }
+  if ((Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\' -Name "Start") -eq 4) { Write-Output "Current status: Disabled"; exit 1 } 
+  else { Write-Output "Current status: Enabled"; exit 0 }
 }
