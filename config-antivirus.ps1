1ce5f47e952bae39f220ed1d471a77f14c48d75c
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 10:09:27 +0100

    Update config-antivirus.ps1

--- config-antivirus.ps1
+++ config-antivirus.ps1
@@ -7,15 +7,16 @@ Param(
 
 if($enable) {
   Set-MpPreference -DisableRealtimeMonitoring $false
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Warning! Realtime monitoring is DISABLED"; exit 1 	}
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "WARNING! Realtime monitoring is DISABLED"; exit 1 	}
   else { Write-Output "Realtime monitoring is ENABLED"; exit 0 }
 
 } elseif($disable) {
-  Set-MpPreference -DisableRealtimeMonitoring $false
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Warning! Realtime monitoring is DISABLED"; exit 1 	}
-  else { Write-Output "Realtime monitoring is ENABLED"; exit 0 }
+  Set-MpPreference -DisableRealtimeMonitoring $true
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime monitoring is DISABLED"; exit 0	}
+  else { Write-Output "WARMIMG! Realtime monitoring is ENABLED"; exit 1 }
+
 } else {
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime monitoring is DISABLED"; exit 1 	}
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime monitoring is DISABLED"; exit 1	}
   else { Write-Output "Realtime monitoring is ENABLED"; exit 0 }
 }
 
