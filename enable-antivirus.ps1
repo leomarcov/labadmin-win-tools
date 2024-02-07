ffad131a3a13d93ebac2c840b72b55c6049e03a3
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 10:38:39 +0100

    Delete enable-antivirus.ps1

--- enable-antivirus.ps1
+++
@@ -1,7 +0,0 @@
-#Requires -RunAsAdministrator
-
-Write-Host -NoNewline  "Enabling antivirus"
-Set-MpPreference -DisableRealtimeMonitoring $false
-if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Host -ForegroundColor red "`t`t`t[FAIL]"; exit 1	}
-else { Write-Host -ForegroundColor green "`t`t`t[OK]"; exit 0 }
-}
