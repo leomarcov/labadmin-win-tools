58314abb3937802cc19949f8fc3028d9057170ae
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 13:14:59 +0100

    Update install-remote-exe.ps1

--- install-remote-exe.ps1
+++ install-remote-exe.ps1
@@ -1,20 +1,28 @@
 #Requires -RunAsAdministrator
 Param(
-  [parameter(Mandatory=$true)]
   [URI]$URL,
   [parameter(Mandatory=$true)]
   [String]$filename,
   [Switch]$downloadOverride,
-  [Switch]$removeDownload
+  [Switch]$removeDownload,
+  [String]$argumentList
 )
 
 #### CONFIG VARIABLES
 $downloadsPath="${ENV:ALLUSERSPROFILE}\labadmin\downloads"                                    # Labadmin Downloads base directory
+$defaultArguments='/S /v /qn'
 
+if(!$argumentList) { $argumentList=$defaultArguments }
 
 # Create download folder if not exists
 if (-not (Test-Path -LiteralPath $downloadsPath -PathType Container)) {	New-Item -ItemType Directory -Path $downloadsPath }   
 $filePath="${downloadsPath}\${filename}"                                                  # File to download path
+
+# if no URL check if file exists
+if(!url -AND !(Test-Path -LiteralPath $filePath -PathType Leaf)) {
+    Write-Error "ERROR: File $filename not found"
+    exit 1
+}
 
 # Download
 if($downloadOverride -OR !(Test-Path -LiteralPath $filePath -PathType Leaf)) {
@@ -26,7 +34,7 @@ if($downloadOverride -OR !(Test-Path -LiteralPath $filePath -PathType Leaf)) {
 
 # Install
 Write-Output "Installing in silent mode: $filePath"
-Start-Process -FilePath $filePath -ArgumentList '/S','/v','/qn' -Verb runas -Wait
+Start-Process -FilePath $filePath -ArgumentList $argumentList -Verb runas -Wait
 $lec=$LASTEXITCODE
 Write-Output "Exit status: $? ($lec)"
 
