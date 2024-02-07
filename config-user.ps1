37d6e35fc4e09e0a53bbd965175e7cc09c2a22e0
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-07 09:54:24 +0100

    Update config-user.ps1

--- config-user.ps1
+++ config-user.ps1
@@ -2,22 +2,26 @@
 
 Param(
   [Parameter(Mandatory)]
-  [String]$User,
+  [String]$UserName,
   [Switch]$Hide,
   [Switch]$Unhide,
   [Switch]$Disable,
   [Switch]$Enable
 )
 
+Get-LocalUser -Name $UserName -ErrorAction Stop | Out-Null
+
 if($Hide) {
-  New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Force | New-ItemProperty -Name $user -Value 0 -PropertyType DWord -ForceNam
+  New-Item 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Force | New-ItemProperty -Name $UserName -Value 0 -PropertyType DWord -Force
 }
 if($Unhide) {
-	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $user -Force
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name $UserName -Force
 }
 if($Disable) {
-	Disable-LocalUser -Name $user
+	Disable-LocalUser -Name $UserName
+    Get-LocalUser -Name $UserName
 }
 if($Enable) {
-	Enable-LocalUser -Name $user
+	Enable-LocalUser -Name $UserName
+    Get-LocalUser -Name $UserName
 }
