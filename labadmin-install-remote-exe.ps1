93f4b0fb8d78bf3d5dc9ad706c7094a61bf94739
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-08 10:09:10 +0100

    Update labadmin-install-remote-exe.ps1

--- labadmin-install-remote-exe.ps1
+++ labadmin-install-remote-exe.ps1
@@ -1,30 +1,34 @@
 #Requires -RunAsAdministrator
 Param(
   [parameter(Mandatory=$true)]
-  [String]$fileName,                 # Filename of installer file
-  [String]$MD5,                   # MD5 to check integrity install file (if match not download)
+  [String]$fileName,                  # Filename of installer file
+  [String]$MD5,                       # MD5 to check integrity install file (if match not download)
   [URI]$URL,                          # URL from download (only download if file not exists and MD5 match)
   [Switch]$forceDownload,             # Force download and override install file
+  [String]$destinationPath,	          # Optional folder to download instead of labadmin base download  
   [Switch]$removeInstaller,           # Remove install file after installation
   [String]$argumentList               # Optional argument list to silent installation instead of default: "/S /v /qn"
 )
 
 #### CONFIG VARIABLES
-$installerPath="${ENV:ALLUSERSPROFILE}\labadmin\downloads\${fileName}"
+$labadminDownloadsPath="${ENV:ALLUSERSPROFILE}\labadmin\downloads"}
 $defaultArguments='/S /v /qn'
+
 if(!$argumentList) { $argumentList=$defaultArguments }
+if(!$destinationPath) { $destinationPath=$labadminDownloadsPath}
+$filePath="${destinationPath}\${fileName}"    
 
 # CALL labadmin-download-file.ps1
 $PSBoundParameters.Remove("removeInstaller") | Out-Null; $PSBoundParameters.Remove("argumentList") | Out-Null
 & "${PSScriptRoot}\labadmin-download-file.ps1" @$PSBoundParameters
 
 # INSTALL
-Write-Output "Installing in silent mode: $installerPath"
-Start-Process -FilePath $installerPath -ArgumentList $argumentList -Verb runas -Wait
+Write-Output "Installing in silent mode: $filePath"
+Start-Process -FilePath $filePath -ArgumentList $argumentList -Verb runas -Wait
 $lec=$LASTEXITCODE
 Write-Output "Exit status: $? ($lec)"
 
 # REMOVE
-if($removeInstaller) { Remove-Item -Force $installerPath }
+if($removeInstaller) { Remove-Item -Force $filePath }
 
 exit $lec
