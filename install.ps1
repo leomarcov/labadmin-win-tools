fe83cabaa66d1e32fff944da9388ec7c423e784a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-08 02:55:05 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -15,3 +15,12 @@ Move-Item -Path "${install_path}\labadmin-win-tools-main\*.ps1" -Destination $in
 Remove-Item -LiteralPath "${install_path}\main.zip" -Force
 Remove-Item -LiteralPath "${install_path}\labadmin-win-tools-main" -Force -Recurse
 
+# Update PATH
+$current_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+# Check if install_path is already in PATH
+if(!($current_path.Split(';') | Where-Object { $_ -eq $install_path })){ 
+  $new_path = "$current_path;${install_path}"
+  Write-Output "Adding $install_path to ENVIRONMENT PATH variable"
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
+}
+
