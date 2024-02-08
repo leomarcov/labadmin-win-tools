ed7f59a359f511feaad9892c7ca88934c5ada5a1
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-08 18:14:34 +0100

    Update labadmin-uninstall-program.ps1

--- labadmin-uninstall-program.ps1
+++ labadmin-uninstall-program.ps1
@@ -1,9 +1,27 @@
 #Requires -RunAsAdministrator
 Param(
-  [String]$Name,                  # Name of program
-  [String]$ID,                     # Identifying Number of program
+  [String]$literalName,                  # Name of program
   [Switch]$List
 )
 
-if($list) { Get-Package -Provider Programs -IncludeWindowsInstaller }
+$name=$literalName
 
+if($list) { Get-Package; exit }
+if(!(Get-Package $name)) { Write-Error "Cant find installed package $name"; exit 1 }
+
+# TRY UNINSTALL: WmiObject
+$app=Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $name }
+if($app) { $app.Uninstall(); if($?) { exit 0 } }
+
+# TRY UNINSTALL: Uninstall-Packate
+$app=Get-Package $name
+if($app) {
+  Uninstall-Package -Name $name -Force
+  if(!(Get-Package $name)) { exit 0 }
+}
+
+# TRY REGEDIT uninstall
+$app=gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_.DisplayName -eq $name }
+if($app) {
+  $uninstallPath=$app.UninstallString
+}
