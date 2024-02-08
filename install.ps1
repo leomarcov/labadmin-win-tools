649bcaf22108270ebc7699121155d475007f503a
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-08 01:43:37 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,13 +4,14 @@ $install_path=$ENV:ProgramFiles+"\labadmin\labadmin-win-tools\"
 $url="https://github.com/leomarcov/labadmin-win-tools/archive/refs/heads/main.zip"
 
 # Create folder
-New-Item -ItemType Directory -Force -Path $install_path | Out-Null
+if(!(Test-Path -LiteralPath $install_path -PathType Container)) { New-Item -ItemType Directory -Force -Path $install_path | Out-Null }
+else { Remove-Item -LiteralPath ${install_path} -Force -Recurse }
 
 # Download repository
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri ${url} -OutFile "${install_path}\main.zip" -ErrorAction Stop
 Expand-Archive -LiteralPath "${install_path}\main.zip" -DestinationPath $install_path -Force -ErrorAction Stop
 Move-Item -Path "${install_path}\labadmin-win-tools-main\*.ps1" -Destination $install_path -ErrorAction Stop
-Remove-Item -LiteralPath "${install_path}\main.zip" -Force -Recurse
+Remove-Item -LiteralPath "${install_path}\main.zip" -Force
 Remove-Item -LiteralPath "${install_path}\labadmin-win-tools-main" -Force -Recurse
 
