3624a53e70c5dc595107838d441df840ed6e1810
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-08 11:13:48 +0100

    Update labadmin-uninstall-program.ps1

--- labadmin-uninstall-program.ps1
+++ labadmin-uninstall-program.ps1
@@ -1,1 +1,9 @@
+#Requires -RunAsAdministrator
+Param(
+  [String]$Name,                  # Name of program
+  [String]$ID,                     # Identifying Number of program
+  [Switch]$List
+)
+
+if($list) { Get-Package -Provider Programs -IncludeWindowsInstaller }
 
