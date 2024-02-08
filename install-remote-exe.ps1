5003e071d8516e76b9247f0cffa8e2c5bab89ef7
leomarcov <32820131+leomarcov@users.noreply.github.com>
2024-02-08 01:17:32 +0100

    Update install-remote-exe.ps1

--- install-remote-exe.ps1
+++ install-remote-exe.ps1
@@ -1,45 +1,21 @@
 #Requires -RunAsAdministrator
 Param(
   [parameter(Mandatory=$true)]
-  [String]$filename,                  # filename of installer file
-  [URI]$URL,                          # URL from download (only download if file not exists and MD5 match)
-  [Switch]$forceDownload,             # force download and override install file
-  [Switch]$removeInstaller,           # remove install file after installation
+  [String]$fileNname,                  # Filename of installer file
   [String]$md5File,                   # MD5 to check integrity install file (if match not download)
+  [URI]$URL,                          # URL from download (only download if file not exists and MD5 match)
+  [Switch]$forceDownload,             # Force download and override install file
+  [Switch]$removeInstaller,           # Remove install file after installation
   [String]$argumentList               # Optional argument list to silent installation instead of default: "/S /v /qn"
 )
 
 #### CONFIG VARIABLES
-$downloadsPath="${ENV:ALLUSERSPROFILE}\labadmin\downloads"                                    # Labadmin Downloads base directory
 $defaultArguments='/S /v /qn'
-
-
-# CREATE FOLDERS
-if (-not (Test-Path -LiteralPath $downloadsPath -PathType Container)) {	New-Item -ItemType Directory -Path $downloadsPath }   
-$installerPath="${downloadsPath}\${filename}"                                                  # File to download path
-
-# CHECK PARAMS
 if(!$argumentList) { $argumentList=$defaultArguments }
-if(!$url -AND ($forceDownload -OR $md5File)) { Write-Error "URL param is mandatory when forceDownload or md5File"; exit 1 }
-if(!$forceDownload) {
-	# If previuos downloader installer not found
-	if(!(Test-Path -LiteralPath $installerPath -PathType Leaf)) {
-		if(!url) {  Write-Error "ERROR: File $filename not found"; exit 1 }
-		$forceDownload=$true	
-	# If previuous downloaded installer found
-	} else {
-		if(!$md5File) { $forceDownload=$false }
-		elseif((Get-FileHash $installerPath -Algorithm MD5).Hash -ne $md5file) { Write-Output "MD5 match!"; $forceDownload=$false }
-		else { Write-Output "MD5 not matching! Forced download"; $forceDownload=$true }
-	}
-}
+$installerPath="${ENV:ALLUSERSPROFILE}\labadmin\downloads\${fileName}"
 
 # DOWNLOAD
-if($forceDownload) {
-    Write-Output "Downloading: $installerPath"
-    Invoke-WebRequest -URI $url -outfile ${installerPath} -ErrorAction Stop
-    Write-Output "Download succsessful: $installerPath"
-}
+${PSScriptRoot}\download-file.ps1 -file
 
 # INSTALL
 Write-Output "Installing in silent mode: $installerPath"
