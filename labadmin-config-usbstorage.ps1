d8ca5d0174f81581b87d2b8a1463b7607c474cd6
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-09 19:33:29 +0100

    Update labadmin-config-usbstorage.ps1

--- labadmin-config-usbstorage.ps1
+++ labadmin-config-usbstorage.ps1
@@ -9,22 +9,24 @@
 .PARAMETER Status
     Show current status
 .NOTES
-    File Name      : labadmin-profiles-cleaner.ps1
-    Author         : Leonardo Marco
+    File Name: labadmin-config-usbstorage.ps1
+    Author   : Leonardo Marco
 #>
-
 
 Param(
   [Switch]$Enable,
   [Switch]$Disable
 )
 
+# DISABLE
 if($disable) {
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 4
 
+# ENABLE
 } elseif($enable) {
   Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\" -Name "Start" -Value 3
 
+# STATUS
 } else {
   if ((Get-ItemPropertyValue -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR\' -Name "Start") -eq 4) { Write-Output "Current status: DISABLED"; exit 1 } 
   else { Write-Output "Current status: ENABLED"; exit 0 }
