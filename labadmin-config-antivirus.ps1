9a9279d113796be30a052cf01615b2452f46be91
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-09 17:49:50 +0100

    Update labadmin-config-antivirus.ps1

--- labadmin-config-antivirus.ps1
+++ labadmin-config-antivirus.ps1
@@ -21,17 +21,17 @@ Param(
 
 if($enable) {
   Set-MpPreference -DisableRealtimeMonitoring $false
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "WARNING! Realtime monitoring is DISABLED"; exit 1 	}
-  else { Write-Output "Realtime monitoring is ENABLED"; exit 0 }
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "WARNING! Realtime protection is DISABLED"; exit 1 	}
+  else { Write-Output "Realtime protection is ENABLED"; exit 0 }
 
 } elseif($disable) {
   Set-MpPreference -DisableRealtimeMonitoring $true
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime monitoring is DISABLED"; exit 0	}
-  else { Write-Error "Realtime monitoring is still ENABLED!"; exit 1 }
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime protection is DISABLED"; exit 0	}
+  else { Write-Error "Realtime protection is still ENABLED!"; exit 1 }
 
 } else {
-  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Error "Realtime monitoring is still DISABLED"; exit 1	}
-  else { Write-Output "Realtime monitoring is ENABLED"; exit 0 }
+  if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Error "Current status: DISABLED"; exit 1	}
+  else { Write-Output "Current status: ENABLED"; exit 0 }
 }
 
 
