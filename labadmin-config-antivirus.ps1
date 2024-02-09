9110859354e68be3dc2a13576ccc25208fe6e40b
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-09 19:42:09 +0100

    Update labadmin-config-antivirus.ps1

--- labadmin-config-antivirus.ps1
+++ labadmin-config-antivirus.ps1
@@ -2,33 +2,38 @@
 <#
 .SYNOPSIS
     Enable/disable Windows antivirus real time protection
+    
 .PARAMETER Enabled
     Enable antivirus real time protection
 .PARAMETER Disabled
     Disable antivirus real time protection
 .PARAMETER Status
     Show current status
+
 .NOTES
     File Name: labadmin-config-antivirus.ps1
     Author   : Leonardo Marco
 #>
 
-
 Param(
   [Switch]$Enable,
-  [Switch]$Disable
+  [Switch]$Disable,
+  [Switch]$Status
 )
 
+# ENABLE
 if($enable) {
   Set-MpPreference -DisableRealtimeMonitoring $false
   if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "WARNING! Realtime protection is DISABLED"; exit 1 	}
   else { Write-Output "Realtime protection is ENABLED"; exit 0 }
 
+# DISABLE
 } elseif($disable) {
   Set-MpPreference -DisableRealtimeMonitoring $true
   if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Output "Realtime protection is DISABLED"; exit 0	}
   else { Write-Error "Realtime protection is still ENABLED!"; exit 1 }
 
+# STATUS
 } else {
   if((Get-MpPreference).DisableRealtimeMonitoring) { Write-Error "Current status: DISABLED"; exit 1	}
   else { Write-Output "Current status: ENABLED"; exit 0 }
