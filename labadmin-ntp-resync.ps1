d96ec6f26713dfb56bfea117211dc695f82e6339
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-09 01:40:58 +0100

    Update labadmin-ntp-resync.ps1

--- labadmin-ntp-resync.ps1
+++ labadmin-ntp-resync.ps1
@@ -1,6 +1,7 @@
 #Requires -RunAsAdministrator
 
+$ntpServer="pd.educarm.net 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
 Start-Service w32time
-w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org" /reliable:yes /update
+w32tm /config /syncfromflags:manual /manualpeerlist:"$ntpServer" /reliable:yes /update
 w32tm /resync /force
-w32tm /query /status
+#w32tm /query /status
