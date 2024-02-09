c5d81d5a7c8419c7552624685b32a8ce8722669c
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-09 01:08:31 +0100

    Update labadmin-uninstall-program.ps1

--- labadmin-uninstall-program.ps1
+++ labadmin-uninstall-program.ps1
@@ -1,13 +1,16 @@
 #Requires -RunAsAdministrator
 Param(
   [String]$literalName,                  # Exact name of program to uninstall
+  [String]$argumentList,                 # Optional arguments for uninstall.exe method
   [Switch]$List
 )
 
+# CONFIG VARIABLES
+$argumentsMethods=@("/S", "/S /v`"/qn`"", "/SILENT")      # Unisntall.exe arguments to try
 $name=$literalName
 
 #LIST 
-if($list) { Get-Package | Select-Object -Property Name; exit }
+if($list) {Get-Package | Select-Object -Property Name | Where-Object { $_.Name -match $name }; exit }
 
 # CHECK PACKAGE INSTALLED
 if(!$name) { Write-Error "-literalName param required"; exit 1 }
@@ -29,15 +32,20 @@ if($app) {
   if(!(Get-Package $name -ErrorAction SilentlyContinue)) { Write-Output "Uninstall successful!"; exit 0 }
 }
 
-# TRY REGEDIT uninstall
+# TRY REGEDIT uninstall.exe
 Write-Output "Trying uninstall using Regedit uninstall path..."
 $app=gci "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_.DisplayName -eq $name }
 if(!$app) { $app = gci "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | foreach { gp $_.PSPath } | ? { $_.DisplayName -eq $name } }
 if($app) {
   $uninstallPath=$app.UninstallString.Trim("`"")
-  Write-Output "Executing uninstall: ${uninstallPath} /S"
-  & $uninstallPath "/S"
-  if(!(Get-Package $name -ErrorAction SilentlyContinue)) { Write-Output "Uninstall successful!"; exit 0 }
+  if([System.IO.Path]::GetExtension($uninstallPath) -eq ".exe") {
+    if($argumentList) { $argumentsMethods=@($argumentList)+$argumentsMethods }
+    foreach($arg in $argumentsMethods) {
+      Write-Output "Executing uninstall: ${uninstallPath} ${arg}"
+      Start-Process -FilePath $uninstallPath -ArgumentList $arg -Verb runas -Wait
+      if(!(Get-Package $name -ErrorAction SilentlyContinue)) { Write-Output "Uninstall successful!"; exit 0 }
+    }
+  }
 }
 
 # NO METHOD FOUND!
