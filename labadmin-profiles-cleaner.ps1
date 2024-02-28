7cdadff6c79067cf319773cea7f2568add6eeeed
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-02-28 19:36:30 +0100

    Update labadmin-profiles-cleaner.ps1

--- labadmin-profiles-cleaner.ps1
+++ labadmin-profiles-cleaner.ps1
@@ -5,27 +5,32 @@
     Automated user profiles cleaner
 .DESCRIPTION
     Automated user profiles cleaner for backup and autorestore at startup according scheduled rules
-    Each profile is backup in c:\users\labadmin-profiles-cleaner\ and a <username>.cfg file is generated
-    Profile config file JSON options are:
+    Each profile folder is backup in c:\users\labadmin-profiles-cleaner\ and a <username>.cfg file is generated
+    Profile <username>.cfg file JSON options are:
         cleanAfterDays: Number of days from last clean to next autoclean (0 clean in each reboot, 1 clean every day, etc)
-        skipUser      : Boolean to skip this user from autoclean (skips cleanAfterDays and cleanAllways)
+        skipUser      : Boolean (true or false) to skip this user from autoclean (skips cleanAfterDays and cleanAllways)
         cleanAlways   : Array of relative paths to clean on every call
         lastClean     : Date when last clean was performed
 
 .PARAMETER CreateBackup
-    Backup (or update backup if previos backup exists) users profiles to C:\Users\labadmin-profiles-cleaner\
+    Backup (or update backup if previos backup exists) users profiles to c:\users\labadmin-profiles-cleaner\
     For new backups default <username>.cfg file is generated
     Parameter -Users must be given with list of users to backup
 .PARAMETER RestoreProfiles
     Try to restore profiles according user backup profile config
 .PARAMETER Users
-    List of users to backup/restore
-    With -CreateBackup this parameter is mandatory
-    When restore is optional. If no given all backup profiles stored are used
+    List of users to backup/restore/modify-config
 .PARAMETER Force
     Force profile clean and ommits skipUser and cleanAfterDays config
 .PARAMETER Log
-    Save output to log file in C:\Users\labadmin-profiles-cleaner\log.txt
+    Save output to log file in c:\users\labadmin-profiles-cleaner\log.txt
+.PARAMETER ShowConfig
+	Show all (or -users list) users config
+.PARAMETER showlog
+	Show log file Content
+.PARAMETER ModifyUsersConfig
+	Modify all (or -users list) users config file 
+	Modified values are given with parameters: CleanAfterDays, SkipUser, CleanAllways and LastClean
 
 .NOTES
     File Name: labadmin-profiles-cleaner.ps1
@@ -34,15 +39,15 @@
 
 Param(
   [parameter(Mandatory=$true, ParameterSetName="modifyconfig")]
-  [Switch]$ModifyUserConfig,
+  [Switch]$ModifyUsersConfig,
   [parameter(Mandatory=$false, ParameterSetName="modifyconfig")]
   [Int]$CleanAfterDays,
   [parameter(Mandatory=$false, ParameterSetName="modifyconfig")]
-  [Int]$SkipUser,
+  [String]$SkipUser,
   [parameter(Mandatory=$false, ParameterSetName="modifyconfig")]
   [String[]]$CleanAllways,
   [parameter(Mandatory=$false, ParameterSetName="modifyconfig")]
-  [Date]$LastClean,
+  [DateTime]$LastClean,
   
   [parameter(Mandatory=$true, ParameterSetName="showuser")]
   [Switch]$ShowConfig,
@@ -123,20 +128,20 @@ function RestoreProfiles {
       $user_backup="${backups_path}\${u}"
       $user_conf_file="${backups_path}\$u.cfg"
 
-      # CHECK BACKUP FOLDER
+      # Check backup folder
       if(!(Test-Path $user_backup))  { Write-Output "WARNING! Folder $user_backup not exists. Skipping user $u"; continue }
       
-      # GET USER CONFIG
+      # Get user config
       $user_conf=@{}; (Get-Content $user_conf_file | ConvertFrom-Json).psobject.properties | Foreach { $user_conf[$_.Name] = $_.Value }
       if($user_conf.cleanAfterDays -isnot [int] -OR !$user_conf.lastClean) {
         Write-Output "WARNING! Invalid config file ${user_conf_file}. Skipping user ${u}"
         continue
       }
 
-      # SKIP USER (only if no force)
+      # Skip user (only if no force)
       if(!$Force -AND $user_conf.skipUser -eq "true") { Write-Output "Skipping user $u (skipUser config file)"; continue }
 
-      # SCHEDULED RESTORE
+      # Scheduled restore
       if($Force -OR (New-TimeSpan -Start ([DateTime]$user_conf.lastClean) -End (Get-Date)).Days -ge $user_conf.cleanAfterDays) {
         Write-Output "Removing user $u profile folder..."
         Remove-Item -Recurse -Force $user_profile
@@ -146,7 +151,7 @@ function RestoreProfiles {
         $user_conf.lastClean=Get-Date -Format "yyy-MM-dd"
         $user_conf | ConvertTo-Json | Out-File $user_conf_file
 
-      # RESTORE ON EVERY CALL
+      # Restore on every call
       } else {
           foreach($d in $user_conf.cleanAllways) { 
               if(!(Test-Path "${user_backup}\$d")) { continue }
@@ -162,8 +167,9 @@ function ShowConfig {
 	foreach($u in $users) {
 		$user_conf_file="${backups_path}\$u.cfg"
 		Write-Output "#### USER: $u #################################################################"
-  		Write-Output "Path: ${user_conf_file}"
+  		Write-Output "File: ${user_conf_file}"
 		Get-Content -Path $user_conf_file
+		Write-Output ""
 	}
 }
 
@@ -171,20 +177,24 @@ function ShowLog {
 	Get-Content -Path $log_path
 }
 
-function ModifyUserConfig {
+function ModifyUsersConfig {
 	if(!$users) { $users=foreach($f in Get-ChildItem $backups_path -filter *.cfg) {$f.basename } }
 	foreach($u in $users) {
+		# Get user config
 		$user_conf_file="${backups_path}\$u.cfg"
 		$user_conf=@{}; (Get-Content $user_conf_file | ConvertFrom-Json).psobject.properties | Foreach { $user_conf[$_.Name] = $_.Value }
 		if($user_conf.cleanAfterDays -isnot [int] -OR !$user_conf.lastClean) {
-        		Write-Output "WARNING! Invalid config file ${user_conf_file}. Skipping user ${u}"
-        		continue
-      		}
+        	Write-Output "WARNING! Invalid config file ${user_conf_file}. Skipping user ${u}"
+        	continue
+      	}
+		
+		# Change user config
 		if($CleanAfterDays) { $user_conf.cleanAfterDays=$CleanAfterDays }
-  		if($SkipUser) { $user_conf.skipUser=$SkipUser }
-    		if($CleanAllways) { }
-      		if($LastClean) { }
+  		if($SkipUser) { if($SkipUser -eq "true") { $user_conf.skipUser=$true } else { $user_conf.skipUser=$false } }
+    	if($CleanAllways) { $user_conf.cleanAllways=$CleanAllways }
+      	if($LastClean) { $user_conf.lastClean=$LastClean.ToString("yyy-MM-dd") }
 
+		# Save user config
  		$user_conf | ConvertTo-Json | Out-File $user_conf_file 
  	} 
 }
@@ -195,8 +205,8 @@ function main {
 	if($BackupProfiles)      	{ BackupProfiles  	}
 	elseif($RestoreProfiles) 	{ RestoreProfiles 	}
  	elseif($ShowConfig) 	 	{ ShowConfig      	}	
-  	elseif($ShowLog)		{ ShowLog         	}
- 	elseif($ModifyUserConfig)	{ ModifyUserConfig	}
+  	elseif($ShowLog)			{ ShowLog         	}
+ 	elseif($ModifyUsersConfig)	{ ModifyUsersConfig	}
 }
 
 # EXEC 
