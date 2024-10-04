9de380915f7253b8bb235aea2ef682126e4fece0
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-04 23:22:58 +0200

    Create labadmin-edit-hostsfile.ps1

---
+++ labadmin-edit-hostsfile.ps1
@@ -0,0 +1,20 @@
+#Requires -RunAsAdministrator
+
+$hosts_path = "$($Env:WinDir)\system32\Drivers\etc\hosts"
+
+
+function AddHost {
+
+}
+
+function RemoveHosts {
+
+}
+
+function CommentHosts {
+
+}
+
+function UncommentHosts {
+
+}
