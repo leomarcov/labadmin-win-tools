f6fbd1d76fad08298262dfda8117b014a72d0167
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-05 12:20:04 +0200

    Update labadmin-config-gateway.ps1

--- labadmin-config-gateway.ps1
+++ labadmin-config-gateway.ps1
@@ -4,38 +4,64 @@
 .SYNOPSIS
     Set gateway address (even if current addres has been get using dhcp)
 
-.PARAMETER gwAddress
+.PARAMETER address
     Gateway address 
-    
+.PARAMETER setGateway
+    Set gateway address and remove old gateway
+.PARAMETER resetGateway
+    Remove gateway and try renew DHCP addresses
+
 .NOTES
     File Name: labadmin-config-gateway.ps1
     Author   : Leonardo Marco
 #>
 
 Param(
-  [ipaddress]$gwAddress
+  [Parameter(Mandatory=$false, ParameterSetName='help')]
+  [Parameter(Mandatory=$true, ParameterSetName='address')] 
+  [ipaddress]$address,
+
+  [Parameter(ParameterSetName='address')
+  [Switch]$setGateway,                      # Set gateway
+  
+  [Parameter(ParameterSetName='address')
+  [Switch]$resetGateway                     # Reset gateway
 )
 
+
+# SETWATEGAY
+if($setGateway) {
+    # Get current gw
+    $gwCurrent=(Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
+
+    # Set new gw
+    $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
+    $wmi.SetGateways($address, 1) | Out-null
+
+    # Show new config
+    Get-NetIPConfiguration
+
+    # EXIT CODE
+    if($address -eq (Get-NetIPConfiguration).IPv4DefaultGateway.NextHop) { 
+        Remove-NetRoute -NextHop $gwCurrent -Confirm:$false -ErrorAction SilentlyContinue | Out-Null    # Remove old gw
+        exit 0 
+    } else { 
+        Write-Output "WANING! Gateway address not chnaged!"; exit 1 
+    }
+
+# RESETGATEWAY
+} elif($resetGateWay) {
+	Remove-NetRoute -NextHop $address -Confirm:$false
+	ipconfig /renew > null
+
 # HELP
-if(!$gwAddress) {
+} else {
   Get-Help $PSCommandPath -Detailed
   exit 1
 }
 
-# GET CURRENT GW
-$gwCurrent=(Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
 
-# SET NEW GW
-$wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
-$wmi.SetGateways($gwAddress, 1) | Out-null
 
-# SHOW NEW CONFIG
-Get-NetIPConfiguration
 
-# EXIT CODE
-if($gwAddress -eq (Get-NetIPConfiguration).IPv4DefaultGateway.NextHop) { 
-    Remove-NetRoute -NextHop $gwCurrent -Confirm:$false -ErrorAction SilentlyContinue | Out-Null    # Remove old gw
-    exit 0 
-} else { 
-    Write-Output "WANING! Gateway address not chnaged!"; exit 1 
-}
+
+
