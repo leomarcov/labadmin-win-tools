d9de4ebac616ff751d71ff1dc53fa669f772fdcc
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-05 10:53:05 +0200

    Update labadmin-edit-hostsfile.ps1

--- labadmin-edit-hostsfile.ps1
+++ labadmin-edit-hostsfile.ps1
@@ -25,32 +25,37 @@ Param(
   [String]$RemoveHosts
 )
 
+#  CONFIG VARIABLES
 $hosts_path = "$($Env:WinDir)\system32\Drivers\etc\hosts"
 $hosts_comment= "# labadmin-edit-hostsfile"
 
+# SHOWHOSTSFILE
 function ShowHostsFile {
   Get-Content $hosts_path
 }
 
+# DENAYHOSTS
 function DenyHosts {
   $DenyHosts = $DenyHosts.Split() | foreach { "127.0.0.1".PadRight(20, " ") + $_.PadRight(40, " ") + "# labadmin-edit-hostsfile" }
   Add-Content -Encoding UTF8  $hosts_path $DenyHosts
 }
 
+# WIPEHOSTSFILE
 function WipeHostsFile {
   $RemoveHosts=$hosts_comment
   RemoveHosts
 }
 
+# REMOVEHOSTS
 function RemoveHosts {
   (Get-Content $hosts_path | Where-Object { -not $_.Contains($RemoveHosts) }) | Out-File -Encoding UTF8 -FilePath $hosts_path
 }
 
 
-if($ShowHostsFile)      	{ ShowHostsFile  	}
-elseif($DenyHosts) 		{ DenyHosts 		}
-elseif($WipeHostsFile)		{ WipeHostsFile		}
+if($ShowHostsFile)      { ShowHostsFile  	}
+elseif($DenyHosts) 		  { DenyHosts 		}
+elseif($WipeHostsFile)	{ WipeHostsFile		}
 elseif($RemoveHosts)		{ RemoveHosts		}
-else 				{ Get-Help $PSCommandPath -Detailed	}
+else 				            { Get-Help $PSCommandPath -Detailed	}
 
 
