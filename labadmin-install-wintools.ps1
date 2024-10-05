047ff9bf1c2f1d9dab1fd7cebaef6aab564f1a1e
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-05 20:53:49 +0200

    Update labadmin-install-wintools.ps1

--- labadmin-install-wintools.ps1
+++ labadmin-install-wintools.ps1
@@ -1,5 +1,14 @@
 #Requires -RunAsAdministrator
 
+<#
+.SYNOPSIS
+    Install/update labadmin wintools from GitHub repository
+    
+.NOTES
+    File Name: labadmin-config-antivirus.ps1
+    Author   : Leonardo Marco
+#>
+
 $install_path=$ENV:ProgramFiles+"\labadmin\labadmin-win-tools\"
 $url="https://github.com/leomarcov/labadmin-win-tools/archive/refs/heads/main.zip"
 
@@ -21,10 +30,10 @@ Remove-Item -LiteralPath "${install_path}\main.zip" -Force
 Remove-Item -LiteralPath "${install_path}\labadmin-win-tools-main" -Force -Recurse
 
 # Update PATH
-Write-Output 'Updating $PATH ...'
 $current_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 # Check if install_path is already in PATH
 if(!($current_path.Split(';') | Where-Object { $_ -eq $install_path })){ 
+  Write-Output 'Updating $PATH ...'
   $new_path = "$current_path;${install_path}"
   Write-Output "Adding $install_path to ENVIRONMENT PATH variable"
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
