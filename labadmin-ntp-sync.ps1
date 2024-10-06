0f3518e40daa2c4590e549d4fe2e2cd91bfb37d3
Leonardo Marco <32820131+leomarcov@users.noreply.github.com>
2024-10-06 11:37:32 +0200

    Update labadmin-ntp-sync.ps1

--- labadmin-ntp-sync.ps1
+++ labadmin-ntp-sync.ps1
@@ -2,43 +2,23 @@
 
 <#
 .SYNOPSIS
-    Force NTP sync and update NTP server list
-
-.PARAMETER ntpServersList
-    List of NTP servers (space separated)
+    Force NTP sync 
     
 .NOTES
     File Name: labadmin-ntp-sync.ps1
     Author   : Leonardo Marco
 #>
 
-Param(
-  [Switch]$updateServers,
-  [String]$serverList,
-  [Switch]$forceSync,
-  [Switch]$status
-)
-
-# CONFIG VARIABLES
-$defaultNTPServerList="time.windows.com time.nist.gov 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
-
 
 Start-Service w32time
+Write-Output "`n#### NTP SYNC ######################################################################"
+w32tm /resync /force
 
-# CONFIG NTP SERVER LIST
-if($updateServers) {
-    if(!$serverList) { $serverList=$defaultNTPServerList }
-    w32tm /config /syncfromflags:manual /manualpeerlist:"$serverList" /reliable:yes /update
-}
-
-# FORCE SYNC
-if($forceSync) {
-    w32tm /resync /force
-}
-
-# STATUS
-Write-Output "`n#### NTP STATUS ############################################################"
+Write-Output "`n#### NTP STATUS ####################################################################"
 w32tm /query /status
+
+
+
 
 
 
